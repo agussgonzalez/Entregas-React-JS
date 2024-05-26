19452ba152ebb6926a8db74f8c59cfe4b30b5aba
Agustin Gonzalez <ag8384464@gmail.com>
2024-05-25 22:14:25 -0300

    progreso

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Carrito } from './Carrito';
 import { NavBar } from './NavBar'
+import logo from'../img/logo.jpg'
 
 const Header = () => {
   return (
     <header className='header'>
-        <img src="./img/logo-midland.jpg" alt="Logo"/>
-        <h1>Midland Shopping</h1>
+      <img className='logo' src={logo} alt="logo" />
+        <h1 className='titulo'>Midland Shopping</h1>
+
         <NavBar />
         <Carrito />
     </header>
