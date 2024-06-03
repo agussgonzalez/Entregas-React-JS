a539db9c44b803be5bfe57cbf367753b723babc7
Agustin Gonzalez <ag8384464@gmail.com>
2024-06-03 16:31:44 -0300

    se agregaron imagenes

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export const Item = ({producto}) => {
   return (
     <div className='div-product'>
-        <img src={producto.imagen} />
+        <img className='img-cards' src={producto.imagen} />
         <h2>{producto.nombre}</h2>
         <p>${producto.precio}</p>
         <p>{producto.descripcion}</p>
@@ -12,4 +12,3 @@ export const Item = ({producto}) => {
     </div>
   )
 }
-
